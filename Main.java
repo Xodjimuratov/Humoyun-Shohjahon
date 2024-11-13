12844470ccd808fe64054037bf4c8b1f32969f94
Humoyun Hamidov <humoyunhamidov288@gmail.com>
2024-11-13 19:51:09 +0300

    added commit

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-
-    }
+   public void salom(){
+       System.out.println("humoyun");
+       System.out.println("humoyun");
+       System.out.println("humoyun");
+   }
 }
